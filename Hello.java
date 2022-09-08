82dadb40b1a9c37627f85e357a8ae91c9e0d741a
Nurjahan Kazi <kazinurjhan@gmail.com>
2022-09-08 15:28:53 +0530

    some changes in hellow.java file

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
   public static void main(String[] arg)
   {
-    System.out.println(" Hello World ");
+    System.out.println(" Hello World , this is first program  ");
+    System.out.pintln(" hi, hello, how are you");
   }
 }
